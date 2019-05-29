c5035e2ff46bb9b6531e5fa7b39bd19a2445789c
vtharini <46223387+vtharini@users.noreply.github.com>
2019-05-29 19:40:41 +0530

    Update `vow.py

--- `vow.py
+++ `vow.py
@@ -2,10 +2,10 @@ ch=input()
 ch.lower()
 li=['a','e','i','o','u']
 if(ord(ch)>=97 and ord(ch)<=122):
-	if ch in li:
-		print("Vowel")
+  if ch in li:
+    print("Vowel")
   else:
-	  print("Consonant")
+	print("Consonant")
 else:
-	print("Invalid")
+  print("Invalid")
   
