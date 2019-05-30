ed10e982770d25d2493304180f36c37e628165eb
vtharini <46223387+vtharini@users.noreply.github.com>
2019-05-30 11:57:40 +0530

    Create armstrong_range.py

---
+++ armstrong_range.py
@@ -0,0 +1,15 @@
+u,v=map(int,input().split())
+for nu in range(u,v+1):
+    temp=nu
+    dig=nu
+    c=0
+    sum=0
+    while(dig!=0):
+        dig=dig//10
+        c+=1
+    while(temp!=0):
+        d=temp%10
+        sum=sum+(d**c)
+        temp//=10
+    if(sum==nu):
+        print(nu)
