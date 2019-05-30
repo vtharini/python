e4ac61b26e9ff0d339d9def9b6c89e684febc626
vtharini <46223387+vtharini@users.noreply.github.com>
2019-05-30 11:54:26 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,16 @@
+nu=int(input())
+temp=nu
+dig=nu
+c=0
+sum=0
+while(dig!=0):
+  dig=dig//10
+  c+=1
+while(temp!=0):
+    d=temp%10
+    sum=sum+(d**c)
+    temp//=10
+if(sum==nu):
+    print("yes")
+else:
+    print("No")
